1a474b7a3d5f930948f9c20cdd9a6f9403ee1d60
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-25 16:48:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,11 @@
 
 def oxford_comma(array)
-  if array.length == 2
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    array[-1].prepend "and "
     array.join(", ")
-    array[-2] << ", and"
-    
-  elsif array.length > 2
-    new_array[-2] << ", and"
-    new_array  = array.join(", ")
-    
-  else
-    array.join()
-  end
 end
-
+endlearn
